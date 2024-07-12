f263bb6ee16af7be6bb3653a3aa6aa343fb6ce5b
Divak2004 <fu_david@outlook.com>
2024-07-12 11:00:35 +0800

    Can simulate one round of rps

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-console.log("Hello world!");
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+var humanScore = 0;
+var computerScore = 0;
+
+playRound(humanSelection, computerSelection);
+
+function playRound(humanChoice, computerChoice) {
+    var result = ""
+    if (humanChoice === computerChoice) {
+        result = "Draw! Both of you played " + capitalizeFirstLetter(humanChoice);
+    } else if ((humanChoice === 'rock' && computerChoice === 'paper') 
+    || (humanChoice === 'paper' && computerChoice === 'scissors')
+    || (humanChoice === 'scissors' && computerChoice === 'rock')) {
+        result = "You lose! " + capitalizeFirstLetter(computerChoice) + " beats " + capitalizeFirstLetter(humanChoice);
+        computerScore++;
+    } else {
+        result = "You win! " + capitalizeFirstLetter(humanChoice) + " beats " + capitalizeFirstLetter(computerChoice);
+        humanScore++;
+    }
+    console.log(result)
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function getComputerChoice() {
     var num = Math.random()
